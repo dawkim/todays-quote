ccabc9a6f90aa406e447fdfc0cbdc3219b071e3f
unknown <dawkim6590@gmail.com>
2025-05-10 23:26:21 +0900

    Feat: 오늘의 명언 기능 추가

--- deploy.ps1
+++
@@ -1,5 +0,0 @@
-cd d:/CODECODE
-git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/dawkim/todays-quote
-git push -u https://dawkim:$env:GITHUB_TOKEN@github.com/dawkim/todays-quote.git main
