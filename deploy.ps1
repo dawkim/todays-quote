7e64ae20a5551ffd6ccb8b2d73fd6e2cc1b600d8
unknown <dawkim6590@gmail.com>
2025-05-10 23:22:39 +0900

    Remove secret from commit history

--- deploy.ps1
+++ deploy.ps1
@@ -2,4 +2,4 @@ cd d:/CODECODE
 git add .
 git commit -m "Initial commit"
 git remote add origin https://github.com/dawkim/todays-quote
-git push -u https://dawkim:$([System.Uri]::EscapeDataString("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"))@github.com/dawkim/todays-quote.git main
+git push -u https://dawkim:$env:GITHUB_TOKEN@github.com/dawkim/todays-quote.git main
